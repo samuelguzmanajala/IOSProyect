83c9961167524de5f5e09c8a67f464d82541ab30
samuel <samuelguzmanajala@gamail.com>
2021-03-03 10:28:07 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/IOSProyect.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/IOSProyect.dir/main.c.obj"
+  "IOSProyect.exe"
+  "IOSProyect.exe.manifest"
+  "IOSProyect.lib"
+  "IOSProyect.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/IOSProyect.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
